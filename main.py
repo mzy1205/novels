cac777ee569a1776a677d9b2388189368ddca560
mzy1205 <872883972@qq.com>
2018-07-03 16:23:33 +0800

    可打包版本v_1.0

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import requests
 import urllib
 import sys
 import re
+import datetime
 
 host = 'http://www.biquge.com.tw'
 
-# _name = input("请输入书名：")
-_name = '超品相师'
+_name = input("请输入书名：")
+# _name = '牧神记'
+f = open(_name+'.txt', 'w')
 _name = urllib.request.quote(_name.encode('gbk'))
 
 search_url = 'http://www.biquge.com.tw/modules/article/soshu.php'
@@ -20,15 +22,28 @@ html_doc = r.text
 soup = BeautifulSoup(html_doc, 'html.parser')
 regexp = re.compile("/(.+?).html")
 links = soup.find_all(href=regexp)
+novel = ''
+# print('开始时间：')
+# print(datetime.datetime.now().strftime('%Y-%m-%%d %H:%M:%S'))
 for link in links:
+    # print('当前链接开始的时间：')
+    # print(datetime.datetime.now().strftime('%Y-%m-%%d %H:%M:%S'))
     href = link.get('href')
-    href += host
+    href = host + href
     chapter = requests.get(href)
     chapter.encoding = 'gbk'
     chapter_doc = chapter.text
     s_soup = BeautifulSoup(chapter_doc, 'html.parser')
+    title = s_soup.find('h1').get_text() + '\r\n'
+    content = s_soup.find('div', id='content').get_text() + '\r\n'
+    # f.write(title + '\n')
+    # f.write(content + '\n')
+    novel = novel + title + content
 
-
-
-
-
+# print('链接循环结束时间，准备写文件：')
+# print(datetime.datetime.now().strftime('%Y-%m-%%d %H:%M:%S'))
+novel = novel.replace(u'\xa0', u' ')
+f.write(novel)
+f.close()
+# print('写文件结束时间：')
+# print(datetime.datetime.now().strftime('%Y-%m-%%d %H:%M:%S'))
